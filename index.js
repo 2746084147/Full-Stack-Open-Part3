df58b69066531a434d7871ab43846080761d06aa
diaozhuo <diaozhuo@shanghaitech.edu.cn>
2025-01-06 13:36:23 +0800

    newest version

--- index.js
+++ index.js
@@ -23,15 +23,17 @@ const logRequestBody = (tokens, request, response) => {
 }
 app.use(morgan(logRequestBody))
 
-const unkonwnEndpoint = (request, response) => {
-  response.status(404).send({error:"unknown endpoint"})
-}
-app.use(unkonwnEndpoint)
+// const unkonwnEndpoint = (request, response) => {
+//   response.status(404).send({error:"unknown endpoint"})
+// }
+// app.use(unkonwnEndpoint)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if(error.name === 'CastError'){
     return response.status(400).send({error:'malformatted id'})
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({error:error.message})
   }
   next(error)
 }
@@ -39,10 +41,13 @@ app.use(errorHandler)
 
 app.get('/info', (request, response) => {
   const time = new Date()
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people.</p>
-    <p>${time}</p>
+  Person.countDocuments({}).then(count => {
+    response.send(`
+      <p>Phonebook has info for ${count} people.</p>
+      <p>${time}</p>
     `)
+  })
+
 })
 
 app.get('/api/persons', (request, response) => {
@@ -66,6 +71,8 @@ app.get('/api/persons/:id', (request, response, next) => {
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
+      console.log('delete success');
+      
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -76,7 +83,7 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
 
   // First check if the name is empty:
-  if(!body.name){
+  if(body.name === undefined){
     return response.status(400).json({
       error:'name missing'
     })
@@ -97,6 +104,23 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savePerson => {
     response.json(savePerson)
   })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    person, 
+    {new: true, runValidators: true, context: 'query'}
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
